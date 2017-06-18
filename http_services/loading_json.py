ceb7fd35cbbe91920c0c342d6e5c000771da1685
weightedEights <poormanjosh@gmail.com>
2017-06-18 17:10:46 -0200

    update directory structure. added loading_json.py skeleton.

---
+++ http_services/loading_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""  Some Description """
+
+import requests
+
+
+def main():
+
+    print("loading_json.py")
+
+
+if __name__ == '__main__':
+    main()
