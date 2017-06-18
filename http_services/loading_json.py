dd75a811b7591a97d4d5a71e0bcd48e5b3630d4a
weightedEights <poormanjosh@gmail.com>
2017-06-18 21:06:55 -0200

    updated loading_json.py with loads and dumps

--- http_services/loading_json.py
+++ http_services/loading_json.py
@@ -3,12 +3,31 @@
 
 """  Some Description """
 
-import requests
+import json
 
 
 def main():
 
-    print("loading_json.py")
+    text_json = '''{
+        "demo": "Processing JSON in Python",
+        "instructor": "Michael",
+        "duration": 5.0    
+        }'''
+
+    data = json.loads(text_json)
+
+    print(type(data), data)
+
+    # instructor = data['instructor']
+    instructor = data.get('instructor', 'Substitute')
+    print("Your instructor is {}.".format(instructor))
+
+    data['instructor'] = "Jeff"
+
+    new_json = json.dumps(data)
+
+    print(type(new_json), new_json)
+
 
 
 if __name__ == '__main__':
