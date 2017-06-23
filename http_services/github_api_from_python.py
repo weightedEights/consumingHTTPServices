24695fe0917eccc16bcc8e19c56d2d3da6843b4f
weightedEights <poormanjosh@gmail.com>
2017-06-23 19:57:14 -0200

    prompt for user, repo input

--- http_services/github_api_from_python.py
+++ http_services/github_api_from_python.py
@@ -8,11 +8,22 @@ import requests, json
 
 
 def main():
+    # user = mikeckennedy
+    # repo = consuming_services_python_demos
+    user, repo = get_repo_info()
 
-    url = 'https://api.github.com/repos/mikeckennedy/consuming_services_python_demos'
+    url = 'https://api.github.com/repos/{}/{}'.format(user, repo)
     resp = requests.get(url)
 
     print(json.dumps(json.loads(resp.text), indent = True))
+
+
+def get_repo_info():
+
+    user = input("What is the username?")
+    repo = input("What is the repo?")
+
+    return user, repo
 
 
 if __name__ == '__main__':
